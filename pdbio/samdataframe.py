9df9219d2d9d4b09dbb88c447e21cf2108c83396
dceoy <dnarsil+github@gmail.com>
2019-08-01 13:33:02 +0900

    Implement BamFileWriter

--- pdbio/samdataframe.py
+++ pdbio/samdataframe.py
@@ -7,6 +7,7 @@ https://github.com/dceoy/pdbio
 import logging
 import os
 import re
+import subprocess
 import sys
 from collections import OrderedDict
 from multiprocessing import cpu_count
@@ -163,3 +164,55 @@ class SamDataFrame(BaseBioDataFrame):
             s.strip().split('\t', maxsplit=2)[2]
             for s in self.run_and_parse_subprocess(args=args)
         ]).astype(np.int32)
+
+
+class BamFileWriter(object):
+    def __init__(self, out_bam_path, samtools, n_thread=1, kb_per_thread=None,
+                 sort=False, by_qname=False, executable='/bin/bash'):
+        self.__logger = logging.getLogger(__name__)
+        self.__bam_path = out_bam_path
+        self.__logger.debug('Write STDIN into BAM: {}'.format(self.__bam_path))
+        args = (
+            '{samtools} view -bS -'
+            + (' | {samtools} sort -@ {t} -T {d} {mmpt} {n} -' if sort else '')
+            + ' -o {bam}'
+        ).format(
+            samtools=samtools, t=n_thread, d=os.path.dirname(self.__bam_path),
+            mmpt=('-m {}K'.format(kb_per_thread) if kb_per_thread else ''),
+            n=('-n' if by_qname else ''), bam=out_bam_path
+        )
+        self.__logger.debug('{0} <= `{1}`'.format(executable, args))
+        self.proc = subprocess.Popen(
+            args=args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, shell=True, executable=executable
+        )
+
+    def write(self, *args, **kwargs):
+        self.proc.stdin.write(*args, **kwargs)
+
+    def close(self):
+        self.__logger.debug('Finish writing BAM: {}'.format(self.__bam_path))
+        self.proc.stdin.close()
+        self.proc.wait()
+        if self.proc.returncode != 0:
+            self.__logger.error(
+                'STDERR from subprocess `{0}`:{1}{2}'.format(
+                    self.proc.args, os.linesep,
+                    self.proc.stderr.decode('utf-8')
+                )
+            )
+            raise subprocess.CalledProcessError(
+                returncode=self.proc.returncode, cmd=self.proc.args,
+                output=self.proc.stdout, stderr=self.proc.stderr
+            )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ex_type, ex_value, trace):
+        args = {'ex_type': ex_type, 'ex_value': ex_value, 'trace': trace}
+        self.__logger.debug('with-statement exit: {}'.format(args))
+        self.close()
+
+    def __del__(self):
+        self.close()
