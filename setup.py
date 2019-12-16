a380769bbd6815148d958080cd21dc3f694d9ad6
dceoy <dnarsil+github@gmail.com>
2019-12-17 03:59:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python
 
-import os
-
 from setuptools import find_packages, setup
 
 from pdbio import __version__
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
-    readme_str = f.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='pdbio',
     version=__version__,
-    description='Pandas-based Data Handler for VCF, BED, and SAM Files',
-    packages=find_packages(),
     author='Daichi Narushima',
     author_email='dnarsil+github@gmail.com',
+    description='Pandas-based Data Handler for VCF, BED, and SAM Files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/dceoy/pdbio',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=['docopt', 'pandas'],
-    entry_points={
-        'console_scripts': ['pdbio=pdbio.cli:main'],
-    },
+    entry_points={'console_scripts': ['pdbio=pdbio.cli:main']},
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'Environment :: Plugins',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ],
-    long_description=readme_str
+    python_requires='>=3.6'
 )
